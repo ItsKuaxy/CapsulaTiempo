0be832f1c6c7d6d1338d57965f626c93cb096213
alber <alber@PCAlberto>
2026-03-08 11:22:15 +0100

    Versión 1.0: Lanzamiento inicial

--- CapsulaTiempo/src/Mensaje.java
+++ CapsulaTiempo/src/Mensaje.java
@@ -2,8 +2,7 @@
 public class Mensaje {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		 System.out.println("Mensaje para el año 2026: ¡Git funciona!");
 	}
 
 }
